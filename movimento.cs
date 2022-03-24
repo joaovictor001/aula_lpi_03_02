c43493e7242dc0e784293206bbc02509d75a71f9
joaovictor001 <74721656+joaovictor001@users.noreply.github.com>
2022-03-24 12:18:24 -0300

    codigos para o unity

---
+++ movimento.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movimento : MonoBehaviour
+{
+
+    public float Speed;
+    public float Jumpforce;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+    }
+        void Move()
+    {
+        Vector3 movement = new Vector3(Input.GetAxis("Horizontal"), 0f, 0f);
+        transform.position += movement * Time.deltaTime*Speed;
+
+
+        float InputAxis = Input.GetAxis("Horizontal");
+
+        if(InputAxis > 0)
+        {
+            transform.eulerAngles = new Vector2(0f, 0f);
+        }
+
+        if(InputAxis < 0)
+        {
+            transform.eulerAngles = new Vector2(0f, 180f);
+        }
+    }
+}
