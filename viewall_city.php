8009018b617cf9267b00b13bdb65996245cf23b0
joaovictor001 <74721656+joaovictor001@users.noreply.github.com>
2022-04-01 08:51:36 -0300

    adicionando arquivos para escolha de cidades

---
+++ viewall_city.php
@@ -0,0 +1,30 @@
+<?php
+include 'connect.php';
+include 'checklogin.php';
+?>
+<table border='1'>
+    <tr>
+        <th>
+            Name
+        </th>
+        <th>
+            Username
+        </th>
+    </tr>
+
+<?php
+$sq="select * from reg";
+$qu=mysqli_query($con,$sq);
+while($f=  mysqli_fetch_assoc($qu)){
+    ?>
+    <tr>
+        <td>
+            <?php echo $f['name']?>
+        </td>
+        <td>
+            <?php echo $f['username']?>
+        </td>
+    </tr>
+    <?php
+}
+?>
